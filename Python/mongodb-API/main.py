7a3ff127e08eba4c751f9e51553a432f3859f863
Majster_247 <dilerdanych@gmail.com>
2022-12-23 14:02:21 +0100

    właśnie mam rozkimne że Południewski to West 😲😲

--- Python/mongodb-API/main.py
+++ Python/mongodb-API/main.py
@@ -7,19 +7,24 @@ import os.path
 import socket
 import uuid
 import re
-
+#import atexit
 
 import pymongo #pip3 install pymongo
 import configparser #pip3 install configparser
 
+
+log_file=[]
+data_host=[]
+
 class Config():
     def __init__(self) -> None:
+        global log_file
         self.configExist=False 
         self.os=0
         self.ip=0
         self.mac_addr=0
 
-        if(os.path.exists("Config.majster")==True):
+        if(os.path.exists("config.majster")==True):
             self.configExist=True
             
     def GetOS(self):
@@ -43,11 +48,30 @@ class Config():
     def GetMAC(self):
         try:
             self.mac_addr=':'.join(re.findall('..', '%012x' % uuid.getnode()))
-            print(time.strftime("%H:%M:%S", time.localtime())+"  "+"Your MAC address: "+self.mac_addr)
+            command=time.strftime("%H:%M:%S", time.localtime())+"  "+"Your MAC address: "+self.mac_addr
+            log_file.append(command+"\n")
+            print(command)
         except:
-             print(time.strftime("%H:%M:%S", time.localtime())+"  "+"Error: couldn't get your MAC address")
+            command=time.strftime("%H:%M:%S", time.localtime())+"  "+"Error: couldn't get your MAC address"
+            log_file.append(command+"\n")
+            print(command)
 
-            
+    def MakeConfig(self):
+        config = configparser.ConfigParser()
+        config['HOST-STATS']['OS']=self.os
+        config['HOST-STATS']['IP']=self.ip
+        config['HOST-STATS']['MAC']=self.mac_addr
+        with open('config.majster', 'w') as configfile:
+            config.write(configfile)
+
+    def GET_DATA(self):
+        config = configparser.ConfigParser()
+        config.read('config.majster')
+        self.os=config['HOST-STATS']['OS']
+        self.ip=config['HOST-STATS']['IP']
+        self.mac_addr=config['HOST-STATS']['MAC']
+
+
 
 
             
@@ -62,6 +86,12 @@ def ConfigCreate():
         config.GetOS()
         config.GetIP()
         config.GetMAC()
+    if(config.configExist==True):
+        config.GET_DATA()
+        data_host.append(config.os)
+        data_host.append(config.ip)
+        data_host.append(config.mac_addr)
+            
 
 ConfigCreate()
 log="API started"
