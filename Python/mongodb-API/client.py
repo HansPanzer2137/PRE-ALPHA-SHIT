8d3f0326b3da417666110224f01464cb1dc38228
Majster na beacie ziomal <htopolski12@gmail.com>
2022-12-26 21:33:12 +0100

    maciuś to jednak debil

--- Python/mongodb-API/client.py
+++ Python/mongodb-API/client.py
@@ -8,21 +8,9 @@ class Client():
         self.port = 2000
 
         self.ClientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.ClientSock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 
-    def connect(self):
-        self.ClientSock.bind((self.ip, self.port))
-
-    def connect2(self):
-        self.ClientSock2.bind((self.ip, 2001))
-
-
-
-app = Client()
-app.connect()
-app.connect2()
 
 
 
