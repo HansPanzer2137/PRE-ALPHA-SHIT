a6aa631ae3510067b882f5093576338da872a5c7
Majster na beacie ziomal <htopolski12@gmail.com>
2022-12-26 19:04:09 +0100

    niga shit gameplay

--- ASM&C-[OS]/bib/SHELL/shell.c
+++ ASM&C-[OS]/bib/SHELL/shell.c
@@ -1,17 +1,7 @@
-#include "SHELL.h"
+#include "shell.h"
 #include "../char.h"
 #include "../types.h"
 #include "../text.h"
 #include "../kernel.h"
 #include "../keyboard.h"
-#include <math.h>
-#include <stdio.h>
-#include <strings.h>
 
-void DONUT(){
-    int k;
-    double sin() ,cos();
-    float A=0, B=0, i, j, z[1760];
-    char b[1760];
-    for(; ; ) {}
-}
