e52009e5d64fbf72ac07d088b50eaf77ad6dbe60
Majster_247 <dilerdanych@gmail.com>
2023-01-08 22:03:01 +0100

    i hate jews

--- Python/[Operation Missword]/main.py
+++ Python/[Operation Missword]/main.py
@@ -3,13 +3,13 @@ import sys
 import time
 import timeit
 
-
+hash = 0
 
 def Crytp42_256(words):
     print("")
 
 
-def Crypt42(words):
+def Crypt42_encode(words):
     wordsBuffer = list(words)
     for i in range(len(wordsBuffer)):
         match wordsBuffer[i]:
@@ -49,18 +49,72 @@ def Crypt42(words):
             case '8': wordsBuffer[i]="///##"
             case '9': wordsBuffer[i]="////#"
             case ' ': wordsBuffer[i]="@"
-        
     print("przed zmianą: "+str(words))
     print("po zmianie: "+str(''.join(wordsBuffer)))
+    global hash
+    hash = str(''.join(wordsBuffer))
 
-message = "Więc jeszcze raz mówię, nic takiego nie robię. Po prostu nie ma żadnego dowodu, czy Hitler wiedział o holokauście, czy nie wiedział. Jeżeli ktoś miałby taki dowód, to jest nagroda 175 tysięcy funtów za taki dowód, niech zatem leci do Londynu i odbiera nagrodę.Takiego dowodu nie ma, a ponieważ sądy nie skazują ludzi bez dowodów, na podstawie pomówień, to niestety istnieje bardzo poważne przypuszczenie, że świętej pamięci Himmler nie raczył poinformować Hitlera o tym, że urządza holokaust, i tyle."
+def Crypt42_decode(mesHash):
+	message = list(mesHash)
+	messageBuff=""
+	mess=[]
+	lenBuff = 0
+	while lenBuff < len(message): #get len of string
+		if(message[lenBuff]=="#" or message[lenBuff]=="/"): #if "#" or "/"
+			for kurwo in range(0, 5): # get i and 4 more chars 
+				messageBuff = messageBuff+message[lenBuff+kurwo] # save it to buffer
+			print("message buff: "+messageBuff) #print buffer
+			match messageBuff:
+				case "#/000": mess.append("a") #if buffer blah blah blah
+				case "/###0": mess.append("b")
+				case "/#/#0": mess.append("c")
+				case "/##00": mess.append("d")
+				case "#0000": mess.append("e")
+				case "##/#0": mess.append("f")
+				case "//#00": mess.append("g")
+				case "####0": mess.append("h")
+				case "##000": mess.append("i")
+				case "#///0": mess.append("j")
+				case "/#/00": mess.append("k")
+				case "#/##0": mess.append("l")
+				case "//000": mess.append("m")
+				case "/#000": mess.append("n")
+				case "///00": mess.append("o")
+				case "#//#0": mess.append("p")
+				case "#/#00": mess.append("r")
+				case "###00": mess.append("s")
+				case "/0000": mess.append("t")
+				case "##/00": mess.append("u")
+				case "#//00": mess.append("w")
+				case "/#//0": mess.append("y")
+				case "//##0": mess.append("z")
+				case "###/0": mess.append("v")
+				case "/##/0": mess.append("x")
+				case "/////": mess.append("0")
+				case "#////": mess.append("1")
+				case "##///": mess.append("2")
+				case "###//": mess.append("3")
+				case "####/": mess.append("4")
+				case "#####": mess.append("5")
+				case "/####": mess.append("6")
+				case "//###": mess.append("7")
+				case "///##": mess.append("8")
+				case "////#": mess.append("9")
+			lenBuff+=5 # add  i+5 
+		if(lenBuff>=len(message)):
+			break
+		if(message[lenBuff]=="@"):
+			mess.append(" ")
+			lenBuff+=1
+		messageBuff=""
+	print("decoded message: "+str(''.join(mess)))
 
-Crypt42(message)
-
-            
-
+hash = '#///0#0000/###0#/000#/000#/000@//##0/##00//##0##000#/#00///00@#//00#/#//+0#//#0##000#0000#/#00/##00#/000#/##0#/000#///0'
+Crypt42_decode(hash)
 
 
-
-
-
+# .......... - 10 znakow
+#		 	- if . or ,:
+#				- get i and 4 more chars:
+#					-if blah blah blah change to "a"-"z" and jump to i+5 
+#			- if @: change [i] on " "
